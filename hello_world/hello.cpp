510abd9b481fd3521a0e941637230a3d61d82531
Bhavain Shah <bhavainshah@gmail.com>
2023-11-09 20:58:42 -0500

    chore: added hello_world func

--- hello_world/hello.cpp
+++ hello_world/hello.cpp
@@ -3,5 +3,23 @@
 #include <iostream>
 
 int main(int argc, char*argv[]) {
+    
+    MPI_Init(&argc, &argv);
 
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    std::cout << "I am process " << world_rank << " out of " << world_size 
+              << ". I am running on machine " << processor_name << "." << std::endl;
+
+    MPI_Finalize();
+    return 0;
+    
 }
